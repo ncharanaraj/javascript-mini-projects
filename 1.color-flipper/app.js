aca9f70cbd25e7b26e1dab1ce136ed78d362aef4
Charanaraj N <goudacharanraj760@gmail.com>
2023-03-29 19:46:07 +0530

    Hex color gerator

--- 1.color-flipper/app.js
+++ 1.color-flipper/app.js
@@ -5,7 +5,6 @@ const color = document.querySelector(".color");
 
 btn.addEventListener("click", function () {
   const randomNumber = getRandom();
-  console.log(randomNumber);
   document.body.style.backgroundColor = colors[randomNumber];
   color.textContent = colors[randomNumber];
 });
--- 1.color-flipper/hex.js
+++ 1.color-flipper/hex.js
@@ -1,1 +1,14 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const color = document.querySelector(".color");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", function () {
+  let randonHexNum = "#";
+  for (var i = 0; i < 6; i++) {
+    randonHexNum += hex[Math.floor(Math.random() * hex.length)];
+  }
+  console.log(randonHexNum);
+  document.body.style.backgroundColor = randonHexNum;
+  color.textContent = randonHexNum;
+});
