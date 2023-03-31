1c41ba1623bdaacf1d6516ede74b4891c88720e4
Charanaraj N <goudacharanraj760@gmail.com>
2023-03-31 22:35:59 +0530

    removed comments

--- 2.counter/app.js
+++ 2.counter/app.js
@@ -6,7 +6,6 @@ const btns = document.querySelectorAll(".btn");
 btns.forEach((item) => {
   item.addEventListener("click", function (e) {
     const styles = e.currentTarget.classList;
-    // console.log(styles);
     if (styles.contains("decrease")) {
       count--;
     } else if (styles.contains("increase")) {
