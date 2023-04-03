f3aafac5fe58f1040e53c0ca801061d8470e03e3
Charanaraj N <goudacharanraj760@gmail.com>
2023-04-03 19:32:34 +0530

    prev and next func

--- 3.review-card/app.js
+++ 3.review-card/app.js
@@ -31,13 +31,13 @@ const reviews = [
 ];
 
 // Select items
-const img = document.getElementById("person-img");
+const image = document.getElementById("person-img");
 const author = document.getElementById("author");
 const job = document.getElementById("job");
 const info = document.getElementById("info");
 
-const prevBtn = document.querySelectorAll(".prev-btn");
-const nextBtn = document.querySelectorAll(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
 const randomBtn = document.querySelector(".random-btn");
 
 // set starting item
@@ -51,8 +51,26 @@ window.addEventListener("DOMContentLoaded", function () {
 // a person showup
 function showPerson(person) {
   const item = reviews[person];
-  img.src = item.img;
+  image.src = item.img;
   author.innerText = item.name;
   job.innerText = item.job;
   info.innerText = item.text;
 }
+
+// prev-btn
+prevBtn.addEventListener("click", function () {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson(currentItem);
+});
+
+// next-btn
+nextBtn.addEventListener("click", function () {
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  showPerson(currentItem);
+});
