a2c6d20e4df56cd748d3eb0f60f10e142a9d761f
Charanaraj N <goudacharanraj760@gmail.com>
2023-04-12 12:52:11 +0530

    feat: controls func

--- 9.video/app.js
+++ 9.video/app.js
@@ -1,3 +1,16 @@
 // MDN
 // The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
 // The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
+
+const videoContainer = document.querySelector(".video-container");
+const switchBtn = document.querySelector(".switch-btn");
+
+switchBtn.addEventListener("click", () => {
+  if (!switchBtn.classList.contains("slide")) {
+    switchBtn.classList.add("slide");
+    videoContainer.pause();
+  } else {
+    switchBtn.classList.remove("slide");
+    videoContainer.play();
+  }
+});
